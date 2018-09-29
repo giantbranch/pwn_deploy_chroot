602a368ee49c233d629f5606940bc729065cd68c
giantbranch <493254599@qq.com>
2018-09-29 13:32:52 +0800

    update catflag and catflag.c

--- catflag.c
+++ catflag.c
@@ -1,11 +1,45 @@
 #include <stdio.h>
- 
-int main()
-{
+#include <string.h>
+#include <unistd.h>
+
+void printflag(){
     FILE *fp = NULL;
     char buff[255];
     fp = fopen("/flag.txt", "r");
     fgets(buff, 255, (FILE*)fp);
     printf("%s\n", buff);
-    fclose(fp);
+    fclose(fp); 
+}
+
+int main(int argc,char **argv)
+{
+    int ch;
+    int flag = 0;
+    while((ch = getopt(argc, argv, "c:")) != -1) {
+        switch(ch) {
+            case 'c':
+                flag = 1;
+                // printf("option c: %s\n", optarg);
+                if (!strncmp(optarg,"sh", 2) || !strncmp(optarg,"/bin/sh", 7))
+                {
+                    printflag();
+                }else{
+                    printf("================================================\n\n");
+                    printf("Only support this commands: sh, /bin/sh\n");
+                    printf("So you must call system(\"sh\") or system(\"/bin/sh\")\n");
+                    printf("================================================\n\n");
+                }
+                break;
+            case '?': // 输入未定义的选项, 都会将该选项的值变为 ?
+                printf("if you use execl, You can only call execl(\"/bin/sh\", \"/bin/sh\", \"-c\", \"/bin/sh\", 0) \n\n");
+                break;
+            default:
+                printf("if you use execl, You can only call execl(\"/bin/sh\", \"/bin/sh\", \"-c\", \"/bin/sh\", 0) \n\n");
+        }
+    }
+    if (flag == 0)
+    {
+        printf("if you use execl, You can only call execl(\"/bin/sh\", \"/bin/sh\", \"-c\", \"/bin/sh\", 0) \n\n");
+    }
+    return 0;
 }
